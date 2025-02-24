e846ea8ae8eb6d5b2eedf658f9e60afd968913f8
DanielGarcíaÁlvaro <131653807+DDDani130@users.noreply.github.com>
2025-02-24 12:17:20 +0100

    cosas

--- C2-Java/src/UD05FlujoDatos/Ejercicio7.java
+++ C2-Java/src/UD05FlujoDatos/Ejercicio7.java
@@ -6,10 +6,10 @@ public class Ejercicio7 {
 		Scanner scanner = new Scanner(System.in);
 		int numero = 1;
         while (numero <= 100) {
-            System.out.println(numero);
+            System.out.println(numero);         
             numero++;
         }
-        scanner.close();
+        scanner.close(); 
 	}
 
 }
