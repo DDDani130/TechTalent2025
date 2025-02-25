c0679a8937c3c60e7fa90b42127003f10130a818
DanielGarcíaÁlvaro <131653807+DDDani130@users.noreply.github.com>
2025-02-25 11:08:33 +0100

    Update Ejercicio11.java

--- C2-Java/src/UD05FlujoDatos/Ejercicio11.java
+++ C2-Java/src/UD05FlujoDatos/Ejercicio11.java
@@ -4,24 +4,27 @@ public class Ejercicio11 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Introduce un día de la semana: ");
+		System.out.print("Introduce un día de la semana en castellano: ");
         String dia = scanner.nextLine().toLowerCase();
         switch (dia) {
             case "lunes":
             case "martes":
-            case "miércoles":
-            case "miercoles":
+            case "miércoles":            
             case "jueves":
             case "viernes":
                 System.out.println("Es un día laboral.");
                 break;
-            case "sábado":
-            case "sabado":
+            case "sábado":            
             case "domingo":
                 System.out.println("No es un día laboral.");
                 break;
+            case "miercoles":
+				System.out.println("Es un día laboral. Te has dejado el acento.");
+				break;
+            case "sabado":
+				System.out.println("No es un día laboral. Te has dejado el acento.");
             default:
-                System.out.println("Día no válido.");
+                System.out.println("No es un día.");
                 break;
         }
         
