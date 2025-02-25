a01c9ab3a3522f1b0f8c48621c0c5243ee3497d2
DanielGarcíaÁlvaro <131653807+DDDani130@users.noreply.github.com>
2025-02-25 13:46:16 +0100

    Update Ejercicio11.java

--- C2-Java/src/UD05FlujoDatos/Ejercicio11.java
+++ C2-Java/src/UD05FlujoDatos/Ejercicio11.java
@@ -6,27 +6,39 @@ public class Ejercicio11 {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Escribe un día de la semana en castellano: ");
         String dia = scanner.nextLine().toLowerCase();
+        System.out.println(dia);
         switch (dia) {
             case "lunes":
             case "martes":
             case "miércoles":            
             case "jueves":
             case "viernes":
-                System.out.println("Es un día laboral.");
+                System.out.println("Es un día laboral.SWITCH");
                 break;
             case "sábado":            
             case "domingo":
                 System.out.println("No es un día laboral.");
-                break;
+                break;	
             case "miercoles":
 				System.out.println("Es un día laboral. Te has dejado el acento.");
 				break;
             case "sabado":
 				System.out.println("No es un día laboral. Te has dejado el acento.");
+				break;
             default:
-                System.out.println("Error.");
+                System.out.println("Error.SWITCH");
                 break;
         }
+        
+        //Quiero generar un if else como este switch
+		if (dia.equals("lunes") || dia.equals("martes") || dia.equals("miércoles") 
+				|| dia.equals("jueves") || dia.equals("viernes")) {
+			System.out.println("Es un día laboral IF.");
+		} else if (dia.equals("sabado") || dia.equals("domingo")) {
+			System.out.println("No es un día laboral.");
+		} else {
+			System.out.println("Error.IF");
+		}
         
         scanner.close();
 	}
