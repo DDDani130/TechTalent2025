77da4af3def1c9fe323ab90801a195942df47c7c
DanielGarcíaÁlvaro <131653807+DDDani130@users.noreply.github.com>
2025-03-04 12:52:56 +0100

    Update Ejercicio02.java

--- C2-Java/src/UD06MétodosArrays/Ejercicio02.java
+++ C2-Java/src/UD06MétodosArrays/Ejercicio02.java
@@ -6,16 +6,34 @@ public class Ejercicio02 {
 
 	public static void main(String[] args) {
 		String cantidadNum = JOptionPane.showInputDialog
-				("Escribe una cantidad de números: ) ").toLowerCase();
+				("Escribe una cantidad de números:  ").toLowerCase();
 		int cantidad = Integer.parseInt(cantidadNum);
+		int[] limites = pedirLimites();
+        int limiteInferior = limites[0];
+        int limiteSuperior = limites[1];
+
+        generarNumerosAleatorios(cantidad, limiteInferior, limiteSuperior);
+    }
+
+    public static int[] pedirLimites() {
+        String limiteInferiorStr = JOptionPane.showInputDialog
+        		("Escribe el límite inferior: ");
+        int limiteInferior = Integer.parseInt(limiteInferiorStr);
+
+        String limiteSuperiorStr = JOptionPane.showInputDialog
+        		("Escribe el límite superior: ");
+        int limiteSuperior = Integer.parseInt(limiteSuperiorStr);
+
+        return new int[]{limiteInferior, limiteSuperior};
+    }
+
+    public static void generarNumerosAleatorios(int cantidad, 
+    		                                    int limiteInferior, int limiteSuperior) {
         Random random = new Random();
         for (int i = 0; i < cantidad; i++) {
-            int numeroAleatorio = random.nextInt();
+            int numeroAleatorio = random.nextInt((limiteSuperior - limiteInferior)
+            		                              + 1) + limiteInferior;
             System.out.println(numeroAleatorio);
-            }
         }
-        public static int generarNumeroAleatorio(int limiteInferior, int limiteSuperior) {
-            Random random = new Random();
-            return random.nextInt((limiteSuperior - limiteInferior) + 1) + limiteInferior;
 	}
 }
