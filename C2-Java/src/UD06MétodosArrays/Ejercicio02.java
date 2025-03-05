068bec9348d2c2318ecd579e55d7f44302568e0c
DanielGarcíaÁlvaro <131653807+DDDani130@users.noreply.github.com>
2025-03-05 12:53:08 +0100

    Update Ejercicio02.java

--- C2-Java/src/UD06MétodosArrays/Ejercicio02.java
+++ C2-Java/src/UD06MétodosArrays/Ejercicio02.java
@@ -1,17 +1,20 @@
 package UD06MétodosArrays;
-import java.util.Random;
 
+import java.util.Random;
 import javax.swing.JOptionPane;
+
 public class Ejercicio02 {
 
 	public static void main(String[] args) {
+		//Cuantos números aleatorios quieres generar
 		String cantidadNum = JOptionPane.showInputDialog
 				("Escribe una cantidad de números:  ").toLowerCase();
 		int cantidad = Integer.parseInt(cantidadNum);
+		//Aqui vamos a pedir los limites inferior y superior con el método pedirLimites()
 		int[] limites = pedirLimites();
         int limiteInferior = limites[0];
         int limiteSuperior = limites[1];
-
+        //El usuario ya te ha dado la cantidad de números y los limites inferior y superior
         generarNumerosAleatorios(cantidad, limiteInferior, limiteSuperior);
     }
 
@@ -29,10 +32,10 @@ public class Ejercicio02 {
 
     public static void generarNumerosAleatorios(int cantidad, 
     		                                    int limiteInferior, int limiteSuperior) {
-        Random random = new Random();
+        Random random = new Random(); //Creamos un objeto Random entre 0 y el 1
         for (int i = 0; i < cantidad; i++) {
-            int numeroAleatorio = random.nextInt((limiteSuperior - limiteInferior)
-            		                              + 1) + limiteInferior;
+            int numeroAleatorio = random
+            		.nextInt((limiteSuperior - limiteInferior) + 1) + limiteInferior;
             System.out.println(numeroAleatorio);
         }
 	}
