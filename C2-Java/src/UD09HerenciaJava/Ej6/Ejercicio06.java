205e33d5cec6a38c3881f64027a5caa9865ec632
DanielGarcíaÁlvaro <131653807+DDDani130@users.noreply.github.com>
2025-04-04 12:28:35 +0200

    separar

--- C2-Java/src/UD09HerenciaJava/Ej6/Ejercicio06.java
+++ C2-Java/src/UD09HerenciaJava/Ej6/Ejercicio06.java
@@ -2,33 +2,7 @@ package UD09HerenciaJava.Ej6;
 
 import java.util.ArrayList;
 
-import UD07ArraylistHashtable.Producto;
 
-class Cine {
-	
-	protected String nombrePeli;
-	protected double precioEntrada;
-	
-	public Cine(String nombrePeli, double precioEntrada) {
-		this.nombrePeli = nombrePeli;
-		this.precioEntrada = 12.99;
-	}
-	
-	public String getNombrePeli() {
-		return nombrePeli;
-	}
-	public void setNombrePeli(String nombrePeli) {
-		this.nombrePeli = nombrePeli;
-	}
-	public double getPrecioEntrada() {
-		return precioEntrada;
-	}
-
-	public void setPrecioEntrada(double precioEntrada) {
-		this.precioEntrada = precioEntrada;
-	}
-
-}
 public class Ejercicio06 {
 
 	public static void main(String[] args) {
@@ -44,69 +18,4 @@ public class Ejercicio06 {
 	}
 
 }
-	class Espectador extends Cine {
-	
-	protected String nombrePersona;
-	protected int edad;
-	protected double dineroPersona;
-	
-	public Espectador(String nombrePersona, int edad, double dineroPersona) {
-        super(null, 0);
-        this.nombrePersona = nombrePersona;
-        this.edad = edad;
-        this.dineroPersona = dineroPersona;
-	}
 
-	public String getNombrePersona() {
-		return nombrePersona;
-	}
-
-	public void setNombrePersona(String nombrePersona) {
-		this.nombrePersona = nombrePersona;
-	}
-
-	public int getEdad() {
-		return edad;
-	}
-
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
-
-	public double getDineroPersona() {
-		return dineroPersona;
-	}
-
-}
-	class Peliculas extends Cine {
-	
-	protected int duracion;
-	protected int edadMinima;
-	protected String director;
-	
-	public Peliculas(String nombrePeli, double precioEntrada, int duracion, int edadMinima, String director) {
-        super(nombrePeli, precioEntrada);
-        this.duracion = duracion;
-        this.edadMinima = edadMinima;
-        this.director = director;		
-	}
-	public int getDuracion() {
-		return duracion;
-	}
-	public void setDuracion(int duracion) {
-		this.duracion = duracion;
-	}
-	public int getEdadMinima() {
-		return edadMinima;
-	}
-	public void setEdadMinima(int edadMinima) {
-		this.edadMinima = edadMinima;
-	}
-	public String getDirector() {
-		return director;
-	}
-	public void setDirector(String director) {
-		this.director = director;
-	}
-
-}
--- C2-Java/src/UD09HerenciaJava/Ej6/Peliculas.java
+++ C2-Java/src/UD09HerenciaJava/Ej6/Peliculas.java
@@ -1,34 +1,34 @@
 package UD09HerenciaJava.Ej6;
 
-//public class Peliculas extends Cine {
-//	
-//	protected int duracion;
-//	protected int edadMinima;
-//	protected String director;
-//	
-//	public Peliculas(String nombrePeli, double precioEntrada, int duracion, int edadMinima, String director) {
-//		super(nombrePeli, precioEntrada);
-//		this.duracion = duracion;
-//		this.edadMinima = edadMinima;
-//		this.director = director;		
-//	}
-//	public int getDuracion() {
-//		return duracion;
-//	}
-//	public void setDuracion(int duracion) {
-//		this.duracion = duracion;
-//	}
-//	public int getEdadMinima() {
-//		return edadMinima;
-//	}
-//	public void setEdadMinima(int edadMinima) {
-//		this.edadMinima = edadMinima;
-//	}
-//	public String getDirector() {
-//		return director;
-//	}
-//	public void setDirector(String director) {
-//		this.director = director;
-//	}
-//
-//}
+class Peliculas extends Cine {
+	
+	protected int duracion;
+	protected int edadMinima;
+	protected String director;
+	
+	public Peliculas(String nombrePeli, double precioEntrada, int duracion, int edadMinima, String director) {
+        super(nombrePeli, precioEntrada);
+        this.duracion = duracion;
+        this.edadMinima = edadMinima;
+        this.director = director;		
+	}
+	public int getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+	public int getEdadMinima() {
+		return edadMinima;
+	}
+	public void setEdadMinima(int edadMinima) {
+		this.edadMinima = edadMinima;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+}
