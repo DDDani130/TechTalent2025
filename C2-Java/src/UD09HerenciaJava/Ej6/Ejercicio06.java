489b3e562cd41f46a60343bd7835839e58828da0
DanielGarcíaÁlvaro <131653807+DDDani130@users.noreply.github.com>
2025-04-04 10:57:47 +0200

    Update Ejercicio06.java

--- C2-Java/src/UD09HerenciaJava/Ej6/Ejercicio06.java
+++ C2-Java/src/UD09HerenciaJava/Ej6/Ejercicio06.java
@@ -1,14 +1,17 @@
 package UD09HerenciaJava.Ej6;
 
+import java.util.ArrayList;
 
-	class Cine {
+import UD07ArraylistHashtable.Producto;
+
+class Cine {
 	
 	protected String nombrePeli;
 	protected double precioEntrada;
 	
 	public Cine(String nombrePeli, double precioEntrada) {
 		this.nombrePeli = nombrePeli;
-		this.precioEntrada = precioEntrada;
+		this.precioEntrada = 12.99;
 	}
 	
 	public String getNombrePeli() {
@@ -29,9 +32,15 @@ package UD09HerenciaJava.Ej6;
 public class Ejercicio06 {
 
 	public static void main(String[] args) {
-		Cine peli = new Peliculas(null, 0, 0, 0, null);
-		Cine espect = new Espectador(null, 0, 0);
-
+		Cine peli = new Peliculas();
+		Cine espect = new Espectador();
+		ArrayList<Pelicula> peliculas = new ArrayList<>();
+		peliculas.add(new Pelicula("Super Mario Bros. La película", 7, 92, "Aaron Horvath"));
+		peliculas.add(new Pelicula("Capitán América: Un nuevo mundo", 12, 118, "Julius Onah"));
+		peliculas.add(new Pelicula("Misión: Imposible 8", 12, 163, "Christopher McQuarrie"));
+		peliculas.add(new Pelicula("Jurassic World: El renacer", 13, 106, "Gareth Edwards"));
+		peliculas.add(new Pelicula("Kraven the Hunter", 18, 126, "J.C. Chandor"));
+		peliculas.add(new Pelicula("Lilo & Stitch (2025)", 7, 108, "Dean Fleischer-Camp"));
 	}
 
 }
