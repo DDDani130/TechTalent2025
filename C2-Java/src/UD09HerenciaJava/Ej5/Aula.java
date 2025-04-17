0cd067ae0aa2216be16a3df400e0ff1b64e603f2
DanielGarcíaÁlvaro <131653807+DDDani130@users.noreply.github.com>
2025-04-17 13:54:04 +0200

    Update Aula.java

--- C2-Java/src/UD09HerenciaJava/Ej5/Aula.java
+++ C2-Java/src/UD09HerenciaJava/Ej5/Aula.java
@@ -4,25 +4,48 @@ import java.util.Random;
 
 public class Aula {
 	
-	public void seleccionarProfesor(Profesores[] profes) {
-	    Random random = new Random();
-	    int index = random.nextInt(profes.length);
-	    Profesores profesorSeleccionado = profes[index];
+	public Profesores seleccionarProfesor(Profesores[] profes) {
+		Random random = new Random();
+	    Profesores profesorSeleccionado = profes[random.nextInt(profes.length)];
+		return profesorSeleccionado;
 	    	    
 	}
-	public void asistenciaEstudiantes(Estudiantes[] estudiantes) {
-	    for (Estudiantes estudiante : estudiantes) {
+	public int asistenciaEstudiantes(Estudiantes[] estudiantes) {
+		if (estudiantes == null || estudiantes.length == 0) return 0;
 
-	        boolean asistira = estudiante.polimorfismoE();
+	    int contadorAsistentes = 0;
+	    for (Estudiantes estudiante : estudiantes) {
+	        if (estudiante.polimorfismoE()) {
+	            contadorAsistentes++;
+	        }
 	    }
+	    return contadorAsistentes;
+	}
+	public void listaAlumnos(Estudiantes[] estudiantes) {
+
+		    System.out.println("Estudiantes aprobados:");
+		    for (Estudiantes estudiante : estudiantes) {
+		        if (estudiante.polimorfismoE() && estudiante.getCalificacion() >= 5) {
+		            System.out.println(estudiante);
+		        }
+		   }
 	}
 	
 	public void clase () {
-		seleccionarProfesor(null);
-		if (polimorfismoP() == false) {
+		Profesores profesorSeleccionado = seleccionarProfesor(null);
+		profesorSeleccionado.polimorfismoP();
+		if (profesorSeleccionado == null || !profesorSeleccionado.polimorfismoP()) {
 			System.out.println("El profesor no asistirá, se suspende la clase.");
 		} else {
 			System.out.println("El profesor asistira a clase");
+			Estudiantes[] estudiantes = new Estudiantes[10];
+			int contadorAsistentes = asistenciaEstudiantes(estudiantes);
+			if (contadorAsistentes < estudiantes.length / 2) {
+				System.out.println("No hay suficientes alumnos, se suspende la clase.");
+	        } else {
+	            System.out.println("La clase se llevará a cabo.");
+	            listaAlumnos(estudiantes);
+	        }
 		}
 	}
 }
