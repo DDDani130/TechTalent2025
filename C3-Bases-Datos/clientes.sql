0a10d475aec1b6a21e4fbe6cce4de46605b32563
DanielGarcíaÁlvaro <131653807+DDDani130@users.noreply.github.com>
2025-05-08 13:47:30 +0200

    Update clientes.sql

--- C3-Bases-Datos/clientes.sql
+++ C3-Bases-Datos/clientes.sql
@@ -1,2 +1,9 @@
 --Crear tabla de clientes
-CREATE
+CREATE DATABASE IF NOT EXISTS ccompraventas;
+USE ccompraventas;
+CREATE TABLE IF NOT EXISTS clientes (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    correo VARCHAR(100) NOT NULL UNIQUE,
+    telefono VARCHAR(15)
+);
