975ec240b3751d920a72aa42a785505d15bf2a52
DanielGarcíaÁlvaro <131653807+DDDani130@users.noreply.github.com>
2025-05-09 10:57:28 +0200

    Update compras_ventas.sql

--- C3-Bases-Datos/compras_ventas.sql
+++ C3-Bases-Datos/compras_ventas.sql
@@ -3,8 +3,10 @@ USE ccompraventas;
 CREATE TABLE IF NOT EXISTS compras_ventas (
     N_pedido INT AUTO_INCREMENT PRIMARY KEY,
     DNI VARCHAR(20) NOT NULL,
-    FOREIGN KEY (DNI) REFERENCES clientes, proveedores(DNI_cliente, DNI_proveedor),
+    FOREIGN KEY (DNI) REFERENCES clientes(DNI_cliente),
+    FOREIGN KEY (DNI) REFERENCES proveedores(DNI_proveedor),
     N_factura INT NOT NULL,
-    FOREIGN KEY (N_factura) REFERENCES facturas_ventas, facturas_compras(N_factura, N_factura),
-    importe DECIMAL(10, 2) NOT NULL
+    FOREIGN KEY (N_factura) REFERENCES facturas_ventas(N_factura),
+    FOREIGN KEY (N_factura) REFERENCES facturas_compras(N_factura),
+    importe DECIMAL(10, 2) NOT NULL,
 );
