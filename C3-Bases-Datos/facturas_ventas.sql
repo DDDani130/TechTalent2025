0b6c2f9696b9a8ad07c71f0e998186fbb9e4a671
DanielGarcíaÁlvaro <131653807+DDDani130@users.noreply.github.com>
2025-05-09 09:17:23 +0200

    Update facturas_ventas.sql

--- C3-Bases-Datos/facturas_ventas.sql
+++ C3-Bases-Datos/facturas_ventas.sql
@@ -2,8 +2,8 @@
 CREATE DATABASE IF NOT EXISTS ccompraventas;
 USE ccompraventas;
 CREATE TABLE IF NOT EXISTS facturas_ventas (
-    DNI_proveedor INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
-    correo VARCHAR(100) NOT NULL UNIQUE,
-    telefono VARCHAR(15)
+    N_factura INT AUTO_INCREMENT PRIMARY KEY,
+    fecha DATE NOT NULL,
+    DNI_cliente VARCHAR(20) NOT NULL,
+    FOREIGN KEY (DNI_cliente) REFERENCES clientes(DNI_cliente),
 );
