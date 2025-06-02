4b3364bcae4ab7db0eee033f3a0bb5cc6aa6cb05
DanielGarcíaÁlvaro <131653807+DDDani130@users.noreply.github.com>
2025-06-02 13:04:55 +0200

    Update Ej2.java

--- C4-UD22-Patron/src/main/java/Ej2.java
+++ C4-UD22-Patron/src/main/java/Ej2.java
@@ -1,4 +1,14 @@
 
 public class Ej2 {
 
+	
 }
+//CREATE TABLE cliente (
+//id int(11) NOT NULL AUTO_INCREMENT,
+//nombre varchar(250) DEFAULT NULL,
+//apellido varchar(250) DEFAULT NULL,
+//direccion varchar(250) DEFAULT NULL,
+//dni int(11) DEFAULT NULL,
+//fecha date DEFAULT NULL,
+//PRIMARY KEY (id)
+//);
